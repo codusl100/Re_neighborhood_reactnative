36efa3dba6cb512eea06e9a98aa3841f4364e51c
백채연 <codusl100@naver.com>
2022-08-14 01:52:58 +0900

    FEAT: home

---
+++ rnproject/screens/UploadScreen.js
@@ -0,0 +1,85 @@
+import React, {useEffect, useRef, useState, useCallback} from 'react';
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Animated,
+  Keyboard,
+  useWindowDimensions,
+} from 'react-native';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import IconRightButton from '../components/IconRightButton';
+
+function UploadScreen() {
+  const route = useRoute();
+  const {res} = route.params || {};
+  const {width} = useWindowDimensions();
+  const animation = useRef(new Animated.Value(width)).current;
+  const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
+  const [description, setDescription] = useState('');
+  const navigation = useNavigation();
+  const onSubmit = useCallback(() => {
+    // TODO: 포스트 작성 로직 구현
+  }, []);
+
+  useEffect(() => {
+    const didShow = Keyboard.addListener('keyboardDidShow', () =>
+      setIsKeyboardOpen(true),
+    );
+    const didHide = Keyboard.addListener('keyboardDidHide', () =>
+      setIsKeyboardOpen(false),
+    );
+
+    return () => {
+      didShow.remove();
+      didHide.remove();
+    };
+  }, []); // 컴포넌트가 화면에 나타날 때 이벤트를 등로갛고, 사라질 때 이벤트를 해제해야 하므로 deps 배열은 비워둠
+
+  useEffect(() => {
+    Animated.timing(animation, {
+      toValue: isKeyboardOpen ? 0 : width,
+      useNativeDriver: false,
+      duration: 150,
+      delay: 100,
+    }).start();
+  }, [isKeyboardOpen, width, animation]);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => <IconRightButton onPress={onSubmit} name="send" />,
+    });
+  }, [navigation, onSubmit]);
+
+  return (
+    <View style={styles.block}>
+      <Animated.Image
+        source={{uri: res.assets[0]?.uri}}
+        style={[styles.image, {height: animation}]}
+        resizeMode="cover"
+      />
+      <TextInput
+        style={styles.input}
+        multiline={true}
+        placeholder="이 사진에 대한 설명을 입력하세요..."
+        textAlignVertical="top"
+        value={description}
+        onChangeText={setDescription}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  block: {flex: 1},
+  image: {width: '100%'},
+  input: {
+    paddingHorizontal: 16,
+    paddingTop: 16,
+    paddingBottom: 16,
+    flex: 1,
+    fontSize: 16,
+  },
+});
+
+export default UploadScreen;
